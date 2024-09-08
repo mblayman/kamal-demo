5e332f31a574ef43c8c6313527b7137c7cadbb6a
Matt Layman <matthewlayman@gmail.com>
2024-09-08 16:45:57 -0400

    Set the db directory.

--- project/settings.py
+++ project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": env.path("DB_DIR", BASE_DIR) / "db.sqlite3",
     }
 }
 
